b67d02d79c06b2d76dca3029155a51c7dd88676d
ytakhs <21127627+ytakhs@users.noreply.github.com>
2022-07-09 23:36:35 +0900

    kebabCase

---
+++ src/kebabCase.ts
@@ -0,0 +1,72 @@
+/*
+  612 - KebabCase
+  -------
+  by Johnson Chu (@johnsoncodehk) #medium #template-literal
+
+  ### Question
+
+  `FooBarBaz` -> `foo-bar-baz`
+
+  > View on GitHub: https://tsch.js.org/612
+*/
+
+
+/* _____________ Your Code Here _____________ */
+
+const map = {
+  A: 'a',
+  B: 'b',
+  C: 'c',
+  D: 'd',
+  E: 'e',
+  F: 'f',
+  G: 'g',
+  H: 'h',
+  I: 'i',
+  J: 'j',
+  K: 'k',
+  L: 'l',
+  M: 'j',
+  N: 'n',
+  O: 'o',
+  P: 'p',
+  Q: 'q',
+  R: 'r',
+  S: 's',
+  T: 't',
+  U: 'u',
+  V: 'v',
+  W: 'w',
+  X: 'x',
+  Y: 'y',
+  Z: 'z',
+} as const;
+
+type _KebabCase<S> = S extends `${infer H}${infer T}` ? H extends keyof typeof map ? `-${typeof map[H]}${_KebabCase<T>}` : `${H}${_KebabCase<T>}` : S;
+type KebabCase<S> = _KebabCase<S> extends `-${infer Body}` ? Body extends '' | '-' ? `-${Body}` : `${Body}` : _KebabCase<S>;
+
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<KebabCase<'FooBarBaz'>, 'foo-bar-baz'>>,
+  Expect<Equal<KebabCase<'fooBarBaz'>, 'foo-bar-baz'>>,
+  Expect<Equal<KebabCase<'foo-bar'>, 'foo-bar'>>,
+  Expect<Equal<KebabCase<'foo_bar'>, 'foo_bar'>>,
+  Expect<Equal<KebabCase<'Foo-Bar'>, 'foo--bar'>>,
+  Expect<Equal<KebabCase<'ABC'>, 'a-b-c'>>,
+  Expect<Equal<KebabCase<'-'>, '-'>>,
+  Expect<Equal<KebabCase<''>, ''>>,
+  Expect<Equal<KebabCase<'😎'>, '😎'>>,
+]
+
+
+
+/* _____________ Further Steps _____________ */
+/*
+  > Share your solutions: https://tsch.js.org/612/answer
+  > View solutions: https://tsch.js.org/612/solutions
+  > More Challenges: https://tsch.js.org
+*/
+
